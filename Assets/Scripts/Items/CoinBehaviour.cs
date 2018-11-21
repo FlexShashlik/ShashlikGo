0190d897049b2de19637d45804b1da068248f1cc
Sevada Saakyan <sevada992@gmail.com>
2018-11-22 01:21:58 +0300

    Added coin prefab.

---
+++ Assets/Scripts/Items/CoinBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CoinBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float m_RoundSpeedFactor; 
+
+    void Update()
+    {
+        gameObject.transform.Rotate
+            (
+                new Vector3(0f, 0f, m_RoundSpeedFactor * Time.deltaTime)
+            );
+    }
+}
